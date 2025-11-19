a740dfb923b5ec9f8c6758ac7196694cb2396bd7
Іра Тімощенко <tymoschenkoira@gmail.com>
2025-11-19 11:54:12 +0200

    Add project files.

---
+++ MyForm.h
@@ -0,0 +1,252 @@
+#pragma once
+#using <Microsoft.VisualBasic.dll>
+
+using namespace System::Collections::Generic;
+namespace lb10 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->ForeColor = System::Drawing::Color::ForestGreen;
+			this->label1->Location = System::Drawing::Point(98, 32);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(654, 25);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Створити чергу символів. Визначити кількість голосних літер.";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.8F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->ForeColor = System::Drawing::Color::ForestGreen;
+			this->label2->Location = System::Drawing::Point(12, 149);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(207, 22);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Введіть розмір черги";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(259, 148);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(142, 27);
+			this->textBox1->TabIndex = 2;
+			// 
+			// button1
+			// 
+			this->button1->BackColor = System::Drawing::Color::ForestGreen;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->ForeColor = System::Drawing::Color::Ivory;
+			this->button1->Location = System::Drawing::Point(621, 125);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(200, 38);
+			this->button1->TabIndex = 3;
+			this->button1->Text = L"Додати елемент";
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::ForestGreen;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->ForeColor = System::Drawing::Color::Ivory;
+			this->button2->Location = System::Drawing::Point(621, 179);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(200, 38);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"Видалити елемент";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::ForestGreen;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button3->ForeColor = System::Drawing::Color::Ivory;
+			this->button3->Location = System::Drawing::Point(187, 262);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(200, 38);
+			this->button3->TabIndex = 5;
+			this->button3->Text = L"Визначити";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// listBox1
+			// 
+			this->listBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 20;
+			this->listBox1->Location = System::Drawing::Point(424, 87);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(158, 244);
+			this->listBox1->TabIndex = 6;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::Ivory;
+			this->ClientSize = System::Drawing::Size(848, 454);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+		System::Collections::Generic::Queue<wchar_t> myQueue1;
+		bool isQueueInitialized = false;
+
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		if (!isQueueInitialized)
+		{
+			isQueueInitialized = true;
+		}
+
+		int size = Convert::ToInt32(textBox1->Text);
+
+		if (myQueue1.Count < size)
+		{
+			String^ valueInput =
+				Microsoft::VisualBasic::Interaction::InputBox(
+					"Введіть елемент черги (один символ):",
+					"Додавання елементу",
+					"");
+
+			if (valueInput == "")
+			{
+				return;
+			}
+
+			wchar_t inputChar = valueInput[0];
+
+			myQueue1.Enqueue(inputChar);
+
+			listBox1->Items->Add(inputChar.ToString());
+		}
+		else
+		{
+
+			listBox1->Items->Add("Досягнуто ліміту символів");
+		}
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (isQueueInitialized)
+	{
+		if (myQueue1.Count > 0)
+		{
+			myQueue1.Dequeue();
+
+			listBox1->Items->Clear();
+
+			for each (wchar_t value in myQueue1)
+			{
+				listBox1->Items->Add(value.ToString());
+			}
+		}
+		else
+		{
+			listBox1->Items->Add("Черга порожня");
+		}
+	}
+	else
+	{
+		listBox1->Items->Add("Черга ще не була ініціалізована");
+	}
+
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	int count = 0;
+
+	for each (wchar_t letter in myQueue1)
+	{
+		if (letter == L'a' || letter == L'e' || letter == L'i' || letter == L'o' || letter == L'u' || letter == L'y' ||
+			letter == L'A' || letter == L'E' || letter == L'I' || letter == L'O' || letter == L'U' || letter == L'Y')
+		{
+			count++;
+		}
+	}
+
+	MessageBox::Show("Кількість голосних літер у черзі: " + count.ToString());
+}
+};
+}
